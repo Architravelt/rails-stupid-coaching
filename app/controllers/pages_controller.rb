3264dd218f42f35ebb80cb9d266b42b824ed059d
Clare Amos <clare.amos@gmail.com>
2021-07-10 14:38:30 +0100

    stupid coach working

---
+++ app/controllers/pages_controller.rb
@@ -0,0 +1,20 @@
+class PagesController < ApplicationController
+  def about
+  end
+
+  def ask
+  end
+
+  def answer
+    @question = params[:question]
+    @answer = ''
+
+    if @question.ends_with?('?')
+      @answer = 'Silly question, get dressed and go to work!'
+    elsif @question == 'I am going to work'
+      @answer = 'Great!'
+    else
+      @answer = 'I don\'t care, get dressed and go to work!'
+    end
+  end
+end
